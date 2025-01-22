fba2158897c08b0ac612b5409af270f010ae4830
jahirxtrap <jahirroman2002@gmail.com>
2025-01-22 00:27:07 -0500

    1.21.4

--- forge/src/main/java/com/jahirtrap/kuromaterials/init/ModMaterials.java
+++ forge/src/main/java/com/jahirtrap/kuromaterials/init/ModMaterials.java
@@ -1,11 +1,14 @@
 package com.jahirtrap.kuromaterials.init;
 
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorType;
+import net.minecraft.world.item.equipment.EquipmentAsset;
+import net.minecraft.world.item.equipment.EquipmentAssets;
 
 import java.util.EnumMap;
 
@@ -14,12 +17,16 @@ import static com.jahirtrap.kuromaterials.KuroMaterialsMod.MODID;
 public class ModMaterials {
     public interface Armor {
         ArmorMaterial ZURITE = new ArmorMaterial(31, createMap(new int[]{3, 6, 8, 3, 11}),
-                16, SoundEvents.ARMOR_EQUIP_NETHERITE, 2.5f, 0.1f, ModTags.Items.ZURITE_INGOTS, ResourceLocation.fromNamespaceAndPath(MODID, "zurite"));
+                16, SoundEvents.ARMOR_EQUIP_NETHERITE, 2.5f, 0.1f, ModTags.Items.ZURITE_INGOTS, createAsset("zurite"));
 
         private static EnumMap<ArmorType, Integer> createMap(int[] values) {
             EnumMap<ArmorType, Integer> enumMap = new EnumMap<>(ArmorType.class);
             for (int i = 0; i < values.length; i++) enumMap.put(ArmorType.values()[i], values[i]);
             return enumMap;
+        }
+
+        private static ResourceKey<EquipmentAsset> createAsset(String name) {
+            return ResourceKey.create(EquipmentAssets.ROOT_ID, ResourceLocation.fromNamespaceAndPath(MODID, name));
         }
     }
 
